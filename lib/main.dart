f015795ef500a0c347c61e3212ab3de3be3cce0b
Ivani_Chizhs <ivanchizhina@gmail.com>
2024-03-13 11:41:16 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+Future<void> main() async {
+
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
