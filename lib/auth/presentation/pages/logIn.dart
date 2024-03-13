281f01ee7a8f6f4a07a575b88d3b3d148b6da1ec
Ivani_Chizhs <ivanchizhina@gmail.com>
2024-03-13 12:34:14 +0300

    signUp

---
+++ lib/auth/presentation/pages/logIn.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ses_son_1_5/common/controllers/passwordController.dart';
+
+import '../../../common/widgets/customTextField.dart';
+
+class LogIn extends StatefulWidget{
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+
+
+  var email = TextEditingController();
+  var password = PasswordTextController();
+  bool isRemember = false;
+
+
+  void onChange(_){
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 78,),
+            Text("Создать аккаунт",
+              style: Theme.of(context).textTheme.titleLarge,),
+            SizedBox(height: 8,),
+            Text("Завершите регистрацию чтобы начать",
+              style: Theme.of(context).textTheme.titleMedium,),
+            CustomTextField(
+              label: "Почта",
+              hint: "**********@gmail.com",
+              controller: email,
+              onChange: onChange,
+            ),
+            CustomTextField(
+                label: "Пароль",
+                hint: "**********",
+                controller: password,
+                onChange: onChange,
+            ),
+            Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: FilledButton(
+                          onPressed: (){},
+                          child: Text("Зарегестрироваться",
+                            style: Theme.of(context).textTheme.titleSmall,)
+                      ),
+                    ),
+                    SizedBox(height: 14,),
+                    RichText(text: TextSpan(
+                        children: [
+                          TextSpan(
+                              text: "У меня уже есть аккаунт!",
+                              style: Theme.of(context).textTheme.titleMedium),
+                          TextSpan(
+                              text: "Войти",
+                              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  color: const Color.fromARGB(255, 106, 139, 249)
+                              )
+                          )
+                        ])),
+                    SizedBox(height: 32,)
+                  ],)
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/common/run.dart
+++ lib/common/run.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ses_son_1_5/auth/presentation/pages/signUpPage.dart';
+import 'package:ses_son_1_5/common/theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-
+  await Supabase.initialize(
+      url: "",
+      anonKey: "");
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: SignUpPage(),
+      theme: theme,
+    );
   }
 
 }
